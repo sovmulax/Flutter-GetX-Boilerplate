12a153afd9c9e44423f7a1b79717bc6b0b361728
Geoffroy Evane <soumailaevane@gmail.com>
2024-03-25 15:22:00 +0000

    chore: template label

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:new_version/main.dart';
+import 'package:templax/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
