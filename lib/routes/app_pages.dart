2bd7e6faccfb18934c5b4e301f2f81826bfb3567
Geoffroy Evane <soumailaevane@gmail.com>
2024-03-26 15:50:52 +0000

    chore: route is ok

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,20 @@
+// import 'package:get/get.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  static const initial = Routes.spash;
+
+  static final routes = [
+    // GetPage(
+    //   name: Routes.spash,
+    //   page: () => SplashScreen(),
+    //   binding: SplashBinding(),
+    // ),
+    // GetPage(
+    //   name: Routes.auth,
+    //   page: () => AuthScreen(),
+    //   binding: AuthBinding(),
+    // ),
+  ];
+}
