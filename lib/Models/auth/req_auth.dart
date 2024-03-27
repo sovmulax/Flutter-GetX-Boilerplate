f9712c4517e46ff9a20b0ecaf7e5912b0232f008
Geoffroy Evane <soumailaevane@gmail.com>
2024-03-27 10:59:03 +0000

    chore: update models

--- lib/Models/auth/req_auth.dart
+++ lib/Models/auth/req_auth.dart
@@ -1,26 +1,16 @@
-import 'dart:convert';
+import 'package:json_annotation/json_annotation.dart';
+part 'req_auth.g.dart';
 
+@JsonSerializable()
 class AuthRequest {
   AuthRequest({
     required this.email,
     required this.password,
   });
 
-  String email;
-  String password;
+  String? email;
+  String? password;
 
-  factory AuthRequest.fromRawJson(String str) =>
-      AuthRequest.fromJson(json.decode(str));
-
-  String toRawJson() => json.encode(toJson());
-
-  factory AuthRequest.fromJson(Map<String, dynamic> json) => AuthRequest(
-        email: json["email"],
-        password: json["password"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "email": email,
-        "password": password,
-      };
+  factory AuthRequest.fromJson(Map<String, dynamic> json) => _$AuthRequestFromJson(json);
+  Map<String, dynamic> toJson() => _$AuthRequestToJson(this);
 }
