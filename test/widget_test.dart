11c865130239429874621a132e057c4b7839b148
sovmulax-pro <soumailaevane@gmail.com>
2025-08-20 15:40:23 +0000

    chore: supprimer les fichiers de données et les dépôts de mock inutilisés

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,57 +1,23 @@
-// // This is a basic Flutter widget test.
-// //
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility that Flutter provides. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:templax/domain/usecases/fetch_headline_use_case.dart';
-// import 'package:templax/domain/usecases/fetch_news_use_case.dart';
-// import 'package:templax/domain/usecases/signup_use_case.dart';
+import 'package:templax/main.dart';
 
-// import 'package:tuple/tuple.dart';
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const App());
 
-// import 'repositories/mock_article_repository.dart';
-// import 'repositories/mock_auth_repository.dart';
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-// void main() {
-//   test('Sign up test', () async {
-//     //Mock input
-//     final registerUserName = "test username";
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
 
-//     //Mock data
-//     final signUpUseCase = SignUpUseCase(MockAuthenticationRepository());
-//     final user = await signUpUseCase.execute(registerUserName);
-
-//     expect(user.username, registerUserName);
-//   });
-
-//   test('Fetch headline test', () async {
-//     final pageSize = 20;
-//     final currentPage = 1;
-
-//     //Mock data
-//     final fetchHeadlineUseCase = FetchHeadlineUseCase(MockArticleRepository());
-//     final paging =
-//         await fetchHeadlineUseCase.execute(Tuple2(currentPage, pageSize));
-
-//     // Verify that data has created.
-//     expect(paging.articles.length, 20);
-//   });
-
-//   test('Fetch news test', () async {
-//     //Mock input
-//     final keyword = "bitcoin";
-//     final pageSize = 20;
-//     final currentPage = 1;
-
-//     //Mock data
-//     final fetchHeadlineUseCase = FetchNewsUseCase(MockArticleRepository());
-//     final paging = await fetchHeadlineUseCase
-//         .execute(Tuple3(keyword, currentPage, pageSize));
-
-//     // Verify that data has created.
-//     expect(paging.articles.length, 3);
-//   });
-// }
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
