0bae6f1f54f82cb56ef4c88c410fd042f37c6f25
sovmulax-pro <soumailaevane@gmail.com>
2025-08-20 15:51:36 +0000

    feat: ajouter le support de la localisation française et mettre à jour les dépendances associées

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:templax/src/exports.dart_exporter.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //await DenpendencyInjection.init();
+  //configLoading();
   await GetStorage.init();
   AppSession.init();
-
-  runApp(const App());
-  //configLoading();
+  initializeDateFormatting('fr_FR', null).then((_) => runApp(const App()));
 }
 
 class App extends StatelessWidget {
@@ -24,11 +24,18 @@ class App extends StatelessWidget {
       enableLog: true,
       initialRoute: Routes.spash,
       defaultTransition: Transition.fade,
-      //getPages: AppPages.routes,
       initialBinding: AppBinding(),
       smartManagement: SmartManagement.keepFactory,
       title: 'Flutter GetX Boilerplate',
       theme: CustomThemeData(context),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('fr', 'FR')],
+      locale: const Locale("fr", "FR"),
+      //getPages: AppPages.routes,
       //locale: TranslationService.locale,
       //fallbackLocale: TranslationService.fallbackLocale,
       //translations: TranslationService(),
